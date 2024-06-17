8e1b4a85ad753610d2aeb358ef3fbc033c257c44
nntoz <yst7630@gmail.com>
2024-06-18 07:28:34 +0900

    🌱 init commit

---
+++ abc/abs/a/a.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+#define ll int64_t
+#define u32 uint32_t
+#define u64 uint64_t
+constexpr int dx[] = {-1,0,1,0};
+constexpr int dy[] = {0,1,0,-1};
+#define _rep1(n) for(int i = 0; i < (n); i++)
+#define _rep2(i,n) for(int i = 0; i < (n); i++)
+#define _rep3(i,a,b) for(int i = (a); i < (b); i++)
+#define _rep4(i,a,b,c) for(int i = (a); i < (b); i += (c))
+#define rep(...) overload(__VA_ARGS__,_rep4,_rep3,_rep2,_rep1)(__VA_ARGS__)
+#define srt(list) sort(list.begin(), list.end());
+#define _rrep1(n) for(int i = (n) - 1; i >= 0; i--)
+#define _rrep2(i,n) for(int i = (n) - 1; i >= 0; i--)
+#define _rrep3(i,a,b) for(int i = (b) - 1; i >= (a); i--)
+#define _rrep4(i,a,b,c) for(int i = (b) - 1; i >= (a); i -= (c))
+#define rrep(...) overload(__VA_ARGS__,_rrep4,_rrep3,_rrep2,_rrep1)(__VA_ARGS__)
+#define str string
+void Main(){
+    int a,b,c;str s;
+    cin>>a;
+    cin>>b>>c;
+    cin>>s;
+    cout<<a+b+c<<" "<<s<<endl;
+}
+
+signed main(){
+    cin.tie(0)->sync_with_stdio(false);
+    cout << setprecision(10) << fixed;
+    Main();
+}
---
+++ abc/abs/g/main.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+#define ll int64_t
+#define u32 uint32_t
+#define u64 uint64_t
+constexpr int dx[] = {-1,0,1,0};
+constexpr int dy[] = {0,1,0,-1};
+#define overload(_1,_2,_3,_4,name,...) name
+#define _rep1(n) for(int i = 0; i < (n); i++)
+#define _rep2(i,n) for(int i = 0; i < (n); i++)
+#define _rep3(i,a,b) for(int i = (a); i < (b); i++)
+#define _rep4(i,a,b,c) for(int i = (a); i < (b); i += (c))
+#define rep(...) overload(__VA_ARGS__,_rep4,_rep3,_rep2,_rep1)(__VA_ARGS__)
+#define srt(list) sort(list.begin(), list.end());
+#define _rrep1(n) for(int i = (n) - 1; i >= 0; i--)
+#define _rrep2(i,n) for(int i = (n) - 1; i >= 0; i--)
+#define _rrep3(i,a,b) for(int i = (b) - 1; i >= (a); i--)
+#define _rrep4(i,a,b,c) for(int i = (b) - 1; i >= (a); i -= (c))
+#define rrep(...) overload(__VA_ARGS__,_rrep4,_rrep3,_rrep2,_rrep1)(__VA_ARGS__)
+#define str string
+void Main(){
+    int N,al=0,bo=0;
+    cin>>N;
+    int a[N];
+    rep(i,N){
+        cin>>a[i];
+    }
+    srt(a);
+    rep(i,N){
+        if (i%2==0){al+=a[i];}
+        else{bo+=a[i+1];}
+    }
+
+}
+
+signed main(){
+    cin.tie(0)->sync_with_stdio(false);
+    cout << setprecision(10) << fixed;
+    Main();
+}
